9d023a09801a2d6a321e32ab7a88601891160b2a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-20 12:07:09 +0530

    ✨ ES6 Modules

---
+++ DAYII/ES6/Modules/Calc.js
@@ -0,0 +1,3 @@
+import { Add, Subtract } from "./Math.js";
+console.log("Addition is : " + Add(10, 20));
+console.log("Difference is : " + Subtract(10, 20));
---
+++ DAYII/ES6/Modules/Math.js
@@ -0,0 +1,9 @@
+export function Add(a, b) {
+  return a + b;
+}
+export function Subtract(a, b) {
+  return a - b;
+}
+function Multiply(a, b) {
+  return a * b;
+}
