115166198c3bb2055e30fb947f51ae04bb2a6b58
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-20 12:25:15 +0530

    ✨ Class Basics

--- DAYII/ES6/Modules/Calc.js
+++ DAYII/ES6/Modules/Calc.js
@@ -1,4 +1,8 @@
-import Product, { Add as Addition, Subtract } from "./Math.js";
-console.log("Addition is : " + Addition(10, 20));
-console.log("Difference is : " + Subtract(10, 20));
-console.log("Product is : " + Product(10, 20));
+import * as MathModule from "./Math.js";
+var { Add, Subtract } = MathModule;
+console.log("Addition is : " + Add(10, 20));
+
+// import Product, { Add as Addition, Subtract } from "./Math.js";
+// console.log("Addition is : " + Addition(10, 20));
+// console.log("Difference is : " + Subtract(10, 20));
+// console.log("Product is : " + Product(10, 20));
